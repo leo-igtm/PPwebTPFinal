24697b1351472c51f8033de7b0616b343d3cddbf
Leonardo Figueroa <62738503+leo-igtm@users.noreply.github.com>
2026-01-31 07:46:31 +0000

    Feat : update proyect Inertiajs+React

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacto</title>
+</head>
+<body>
+     <div class="container mx-auto p-8">
+        <h1 class="text-4xl font-bold text-blue-600">¡Bienvenido a mi sitio!</h1>
+        <p class="mt-4 text-gray-700">Esto es un Contacto.</p>
+    </div>
+
+</body>
+</html>
---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi Página de Inicio</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
+<body>
+    <div class="container mx-auto p-8">
+        <h1 class="text-4xl font-bold text-blue-600">¡Bienvenido a mi sitio!</h1>
+        <p class="mt-4 text-gray-700">Esta es mi primera página en Laravel.</p>
+    </div>
+</body>
+</html>
